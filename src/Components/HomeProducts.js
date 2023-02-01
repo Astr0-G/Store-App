4a849a1416aae7ef860e0501bdbd1d01d9b20849
wg326 <57165451+wg326@users.noreply.github.com>
2023-02-01 15:15:45 -0800

    'update'

--- src/Components/HomeProducts.js
+++ src/Components/HomeProducts.js
@@ -1,14 +1,37 @@
-import { Flex, ScrollView, Text, Pressable } from "native-base"
+import { Flex, ScrollView, Image, Text, Box, Pressable, Heading } from "native-base"
 import React from "react"
 import Colors from "../colors"
 import products from "../data/products"
 function HomeProducts() {
     return (
         <ScrollView flex={1}>
-            <Flex flexWarp="wrap" dirction="row" justifyContent="space-between" px={6}>
+            <Flex flexWrap="wrap" direction="row" justifyContent="space-between" px={6}>
                 {products.map((product) => (
-                    <Text>{product.name}</Text>
-                    // <Pressable key={product._id} w="47%" bg={Colors.white}></Pressable>
+                    // <Text>{product.name}</Text>
+                    <Pressable
+                        key={product._id}
+                        w="47%"
+                        bg={Colors.white}
+                        rounded="md"
+                        shadow={2}
+                        pt={0.3}
+                        my={3}
+                        pb={2}
+                        overflow="hidden"
+                    >
+                        <Image
+                            source={{ uri: product.image }}
+                            alt={product.name}
+                            w="full"
+                            h={24}
+                            resizeMode="contain"
+                        />
+                        <Box px={4} pt={1}>
+                            <Heading size="sm" bold>
+                                {product.price}
+                            </Heading>
+                        </Box>
+                    </Pressable>
                 ))}
             </Flex>
         </ScrollView>
