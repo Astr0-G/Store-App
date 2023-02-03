476c7aab6b8a141b8b2b9c0f42188b4a426458f1
wg326 <57165451+wg326@users.noreply.github.com>
2023-02-03 13:11:41 -0800

    'update'

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,6 +1,7 @@
-import { Box, Heading } from "native-base"
+import { Box, Heading, Text } from "native-base"
 import React from "react"
 import Colors from "../colors"
+import Rating from "./Rating"
 
 function Review({ mt, bg, color, children, onPress }) {
     return (
@@ -10,10 +11,12 @@ function Review({ mt, bg, color, children, onPress }) {
             </Heading>
             {/* IF THERE IS NO REVIEW*/}
             {/* REVIEW*/}
-            <Box p={3} bg={Colors.deepestGray} mt={5} rounded={5}>
+            <Box p={3} bg={Colors.palegreen} mt={5} rounded={5}>
                 <Heading fontSize={15} color={Colors.black}>
                     User Doe
                 </Heading>
+                <Rating value={4} />
+                <Text></Text>
             </Box>
         </Box>
     )
--- src/colors.js
+++ src/colors.js
@@ -1,4 +1,5 @@
 const Colors = {
+    palegreen: "#98FB98",
     darkred: "#ff8000",
     black: "#000000",
     white: "#ffffff",
