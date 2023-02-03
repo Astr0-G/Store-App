11acd0f5e2bb27962605d84bdf23db30632e903d
wg326 <57165451+wg326@users.noreply.github.com>
2023-02-03 14:14:45 -0800

    update

--- src/Components/Notifications/Message.js
+++ src/Components/Notifications/Message.js
@@ -1,10 +1,10 @@
 import { View, Text, Center } from "native-base"
 import React from "react"
 
-function Message({ bg, color, children, size }) {
+function Message({ bg, color, children, size, bold }) {
     return (
         <Center bg={bg} p={4} rounded={5}>
-            <Text color={color} fontSize={size}>
+            <Text color={color} fontSize={size} bold={bold}>
                 {children}
             </Text>
         </Center>
--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,29 +1,67 @@
-import { Box, Heading, Text } from "native-base"
-import React from "react"
+import { Box, CheckIcon, FormControl, Heading, Text, VStack, Select } from "native-base"
+import React, { useState } from "react"
 import Colors from "../colors"
 import Message from "./Notifications/Message"
 import Rating from "./Rating"
 
 function Review() {
+    const [ratings, setRatings] = useState("")
     return (
         <Box my={9}>
             <Heading bold fontSize={15} mb={2}>
                 REVIEW
             </Heading>
             {/* IF THERE IS NO REVIEW*/}
+            <Message color={Colors.black} bg={Colors.main} bold children={"NO REVIEW"} />
             {/* REVIEW*/}
             <Box p={3} bg={Colors.palegreen} mt={5} rounded={5}>
                 <Heading fontSize={15} color={Colors.black}>
                     User Doe
                 </Heading>
                 <Rating value={4} />
-                <Text mb={3}>Jan 12 2023</Text>
+                <Text my={2} fontSize={11}>
+                    Jan 12 2023
+                </Text>
                 <Message
                     color={Colors.black}
                     bg={Colors.white}
-                    size={12}
+                    size={10}
                     children={"This is so useful! Recommend!!!!"}
                 />
+            </Box>
+            {/* WRITE REVIEW */}
+            <Box mt={6}>
+                <Heading fontSize={15} color={Colors.black} mb={4}>
+                    REVIEW THIS PRODUCT
+                </Heading>
+                <VStack space={6}>
+                    <FormControl>
+                        <FormControl.Label
+                            _text={{ fontSize: "12px", fontWeight: "bold", color: Colors.black }}
+                        >
+                            Rating
+                        </FormControl.Label>
+                        <Select
+                            bg={Colors.subGreen}
+                            borderWidth={0}
+                            rounded={5}
+                            py={4}
+                            placeholder="Choose Rate"
+                            _selectedItem={{
+                                bg: Colors.subGreen,
+                                endIcon: <CheckIcon size={5} />,
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                            selectedValue={ratings}
+                            onValueChange={(e) => setRatings(e)}
+                        >
+                            <Select.Item label="Poor" value="1" />
+                            <Select.Item label="Fair" value="2" />
+                            <Select.Item label="Good" value="3" />
+                        </Select>
+                    </FormControl>
+                </VStack>
             </Box>
         </Box>
     )
