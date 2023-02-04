222906615f7d2a2870443c0c33273aa664969a66
wg326 <57165451+wg326@users.noreply.github.com>
2023-02-03 16:14:34 -0800

    update

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar"
-import CartScreen from "./src/Screens/CartScreen"
+import ProfileScreen from "./src/Screens/ProfileScreen"
 import { NativeBaseProvider, Box, Text } from "native-base"
 export default function App() {
     return (
         <NativeBaseProvider>
-            <CartScreen />
+            <ProfileScreen />
         </NativeBaseProvider>
     )
     // return
--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,11 +1,26 @@
-import { Text, View } from "native-base"
+import { Text, Center, Image, Heading } from "native-base"
 import React from "react"
+import Colors from "../colors"
 
 function ProfileScreen() {
     return (
-        <View>
-            <Text>ProfileScreen</Text>
-        </View>
+        <>
+            <Center bg={Colors.palegreen} pt={10} pb={6}>
+                <Image
+                    source={{ uri: "https://i03piccdn.sogoucdn.com/3b190d19e1297e73" }}
+                    alt="profile"
+                    w={24}
+                    h={24}
+                    resizeMode="cover"
+                />
+                <Heading bold fontSize={15} isTruncated my={2} color={Colors.white}>
+                    Admin Doe
+                </Heading>
+                <Text italic fontSize={10} isTruncated my={2} color={Colors.white}>
+                    Joined Dec 12 2022
+                </Text>
+            </Center>
+        </>
     )
 }
 
