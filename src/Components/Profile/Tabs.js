5b5a69116407c0e3f76841d75f9e444fbaf94d82
wg326 <57165451+wg326@users.noreply.github.com>
2023-02-03 16:46:11 -0800

    update

--- src/Components/Profile/Tabs.js
+++ src/Components/Profile/Tabs.js
@@ -1,23 +1,47 @@
-import { View, Text, useWindowDimensions } from "react-native"
+import { View, Text, useWindowDimensions, StyleSheet } from "react-native"
 import React, { useState } from "react"
-import { SceneMap } from "react-native-tab-view"
+import { SceneMap, TabView, TabBar } from "react-native-tab-view"
 import Profile from "./Profile"
 import Order from "./Order"
+import Colors from "../../colors"
 
 const renderScene = SceneMap({
     first: Profile,
     second: Order,
 })
-export default function Tabs() {
+function Tabs() {
     const layout = useWindowDimensions()
     const [index, setIndex] = useState(0)
     const [routes] = useState([
         { key: "first", title: "PROFILE" },
         { key: "second", title: "ORDER" },
     ])
+    const renderTabsBar = (props) => (
+        <TabBar
+            {...props}
+            tabStyle={styles.tabStyle}
+            indicatorStyle={{ backgroundColor: Colors.black }}
+            activeColor={Colors.main}
+            inactiveColor={Colors.white}
+            renderLabel={({ route, color }) => (
+                <Text style={{ color, ...styles.text }}>{route.title}</Text>
+            )}
+        />
+    )
     return (
-        <View>
-            <Text>Tabs</Text>
-        </View>
+        <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            initialLayout={{ width: layout.width }}
+            renderTabBar={renderTabsBar}
+        />
     )
 }
+
+const styles = StyleSheet.create({
+    tabStyle: { backgroundColor: "black" },
+    text: { fontSize: 13, fontWeight: "bold" },
+})
+
+export default Tabs
